6248b1a6274985b5e59f8659560c9dcbfdddcc5a
nattse <118491380+nattse@users.noreply.github.com>
2023-10-06 14:00:40 -0400

    Update sync_gui.py
    Added batch processing feature

--- sync_gui.py
+++ sync_gui.py
@@ -6,9 +6,11 @@ Created on Mon Jul 31 15:35:04 2023
 @author: nathanieltse
 """
 import cv2
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from tqdm import tqdm
 import PyQt5
 from PyQt5.QtGui import QPixmap, QColor, QImage
 from PyQt5.QtCore import Qt
@@ -49,7 +51,7 @@ class FileBrowser(QWidget):
         layout.addLayout(layout_left)
         layout.addLayout(layout_right)
         self.setLayout(layout)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        #self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setWindowTitle("File Browsing Dialog")
         self.setFixedSize(400, 300)
         self.show()
@@ -72,13 +74,14 @@ class FileBrowser(QWidget):
         self.window = sync_window(self.vid_list)
         self.window.show()
 
+"""Creates and contains tabs"""
 class sync_window(QMainWindow):
     def __init__(self, vid_list):
         super().__init__()
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        #self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setWindowTitle('process')
-        #layout = QHBoxLayout()
         self.tabs = QTabWidget()
+        self.tab_dict = {}
         for i in range(vid_list.count()):
             vid_name = vid_list.item(i).text()
             try:
@@ -88,9 +91,27 @@ class sync_window(QMainWindow):
                     short_name = vid_name.split('\\')[-1]
                 except:
                     short_name = vid_name
-            self.tabs.addTab(presync(vid_name), short_name)
+            self.tab_dict[vid_name] = presync(vid_name)
+            self.tabs.addTab(self.tab_dict[vid_name], short_name)
+        self.tabs.addTab(batch_cut(self.tab_dict), 'batch trim')
         self.setCentralWidget(self.tabs)
-    
+
+"""Creates a tab option to process all videos at once"""
+class batch_cut(QWidget):
+    def __init__(self, tab_dict):
+        super().__init__()
+        self.batch_run = QVBoxLayout()
+        self.run = QPushButton('batch trim')
+        self.run.clicked.connect(lambda: self.run_everyone(tab_dict))
+        self.batch_run.addWidget(self.run)
+        self.setLayout(self.batch_run)
+        self.show()
+    def run_everyone(self, tab_dict):
+        for vid_name in tab_dict.keys():
+            tab_dict[vid_name].start_cutting(vid_name)
+
+"""Creates a tab layout for a single video, automatically runs the kmeans program via the sync class, displays
+the results, and presents options to redo or cut the video"""
 class presync(QWidget):
     def __init__(self, vid_name):
         super().__init__()
@@ -167,18 +188,29 @@ class presync(QWidget):
         cap.set(cv2.CAP_PROP_POS_FRAMES,self.sync_window.frame_num)
         print(f'starting frames here: {self.sync_window.frame_num}')
         count = 0
-        while True:
+        dropped_count = 0
+        frame_time = time.time()
+        for a in tqdm(range(length - self.sync_window.frame_num)):
             count += 1
-            print(f'counting frames: {count}')
             flag, frame = cap.read()
             pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+            #print(f'total frames: {length}')
+            #print(f'counting frames: {pos_frame}')
+            if time.time() - frame_time > 5:
+                if pos_frame > (length - 10):
+                    print(f'looks like we got stuck at {pos_frame} out of {length}, breaking')
+                    break
             if not flag:
+                dropped_count += 1
                 continue
-            if pos_frame == length:
+            if pos_frame >= length:
+                print('finished cutting!')
                 break
             self.output.write(frame)
+            frame_time = time.time()
         cap.release()
         self.output.release()
+        print(f'Thought you should know\nDropped {dropped_count} out of {count} frames')
     
     def clearLayout(self, vid_name):
         self.image_label_off.deleteLater()
@@ -218,8 +250,9 @@ class presync(QWidget):
         self.textLabel_on = QLabel('error')
         return
     
+"""The kmeans frame finding process itself"""
 class sync(QWidget):
-    def __init__(self, vid_name):
+    def __init__(self, vid_name, elaborate = False):
         super().__init__()
         
         def frame_color(frame_data):
@@ -257,6 +290,66 @@ class sync(QWidget):
             flag,frame = cap.read()
             cv2.imshow("video", frame)
             pass
+
+        def show_frame_of_interest(framenumber,r,cap):
+            cap.set(cv2.CAP_PROP_POS_FRAMES,framenumber)
+            while True:
+                flag, frame = cap.read()
+                if flag:
+                    frame_of_interest = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+                    break
+            z = frame_of_interest.reshape((-1,3))
+            z = np.float32(z)
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+            k = 2
+            ret,label,center=cv2.kmeans(z,
+                                       k,
+                                       None,
+                                       criteria,
+                                       10,
+                                       cv2.KMEANS_RANDOM_CENTERS)
+            _, counts = np.unique(label, return_counts=True)
+            center = np.uint8(center)
+            res = center[label.flatten()]
+            res = np.flip(res, axis=1) # Since we are planning on showing in plt, need to flip
+            res2 = res.reshape((frame_of_interest.shape)) 
+            ordered_colors = [color for order,color in sorted(zip(counts, center), key = lambda pair: pair[0], reverse=True)]
+            ordered_colors = [np.flip(i) for i in ordered_colors] # Also going to show in plt
+            return label, np.flip(frame_of_interest, axis=2), res2, ordered_colors
+            
+        def show_work(framerange, r, cap):
+            fig, axs = plt.subplots(4, len(range(*framerange)))
+            fig.subplots_adjust(wspace=0, hspace=0)
+            fig.tight_layout()
+            dominant_color = []
+            for count, i in enumerate(range(*framerange)):
+                label, frame_of_interest, res2, colorlist = show_frame_of_interest(i, r, cap)
+                axs[0, count].imshow(frame_of_interest)
+                axs[1, count].imshow(res2)
+                for ind, color in enumerate(colorlist):
+                    color = color / 255
+                    axs[2, count].bar(0, 1 / (3 ** ind), color = color)
+                axs[2, count].set_aspect('equal')
+                for i in [0, 1, 2, 3]:
+                    axs[i, count].set_xticks([])
+                    axs[i, count].set_yticks([])
+                    axs[i, count].xaxis.set_tick_params(labelbottom=False)
+                    axs[i, count].yaxis.set_tick_params(labelleft=False)
+                dominant_color.append(colorlist[0])
+
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+            ret,label,center=cv2.kmeans(np.float32(np.array(dominant_color)),
+                                       2,
+                                       None,
+                                       criteria,
+                                       10,
+                                       cv2.KMEANS_RANDOM_CENTERS)
+            switch_fr = np.where(label!=label[0])[0][0]
+            for count, ax in enumerate(axs[3, :]):
+                if count == switch_fr:
+                    ax.set_facecolor('black')
+                    axs[0, count].set_title(count + framerange[0])
+            plt.show()
         
         cap = cv2.VideoCapture(vid_name)
         length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -301,7 +394,7 @@ class sync(QWidget):
             self.returns = None
             self.frame_num = None
             return
-        switch_frame = np.where(label!=label[1])[0][0] + framerange[0] 
+        switch_frame = np.where(label!=label[0])[0][0] + framerange[0] 
         images_to_check = {}
         images_to_check['signal off'], images_to_check['signal on'] = display_on_off_signal(switch_frame - 1,
                                                                                             switch_frame,
@@ -309,6 +402,9 @@ class sync(QWidget):
                                                                                             cap)
         self.returns = images_to_check
         self.frame_num = switch_frame
+
+        if elaborate:
+            show_work(framerange, r, cap)
         
        
 if __name__ == '__main__':
